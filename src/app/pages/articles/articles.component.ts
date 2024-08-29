167b15d3ada77a17a303b04b8c2c7f5bbaa2caa4
quentin czarnynoga <czarnynoga.quentins@gmail.com>
2024-08-29 15:26:22 +0200

    1

--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ArticleListComponent } from '../../article-list/article-list.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-articles',
@@ -9,5 +10,44 @@ import { ArticleListComponent } from '../../article-list/article-list.component'
   styleUrl: './articles.component.css'
 })
 export class ArticlesComponent {
+
+
+  constructor(private fb: FormBuilder, private articleService: ArticleService){}
+
+  articleForm: FormGroup = this.fb.group({
+    title: ['', Validators.required, Validators.minLength(3)],
+    description:['', Validators.required, Validators.minLength(3)]
+  })
+
+  submitted: boolean = false
+
+  message: boolean = false
+
+  private addArticle(): void
+  {
+    this.articleService.createArticle(this.articleForm.value).subscribe(
+      error: err => {
+        if (!err) {
+          this.message = true
+        } 
+      }
+    )
+    this.articleForm.reset()
+    this.submitted = false
+  }
+
+  public onSubmit(): void
+  {
+    this.submitted = true
+    if (this.articleForm.valid) {
+      
+      this.addArticle()
+
+    }
+  }
+
+  public get form() {
+    return this.articleForm.controls
+  }
 
 }
